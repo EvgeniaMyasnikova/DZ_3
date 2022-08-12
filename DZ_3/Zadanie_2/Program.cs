e4fd82c304c5bef1442be7e9a03dd6cf0de00972
Evgenia <mem1410@yandex.ru>
2022-08-12 17:29:10 +0500

    Задача 21

--- DZ_3/Zadanie_2/Program.cs
+++ DZ_3/Zadanie_2/Program.cs
@@ -6,3 +6,21 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
+Console.Write("Введите координату x точки A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки A: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату x точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+Console.WriteLine(result);
+
+
